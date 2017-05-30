5e77682756dfa2fdbf659434232613d1eefc37df
Sahaana Suri <sahaana.suri@gmail.com>
2017-05-30 16:35:50 -0700

    organization

---
+++ dropExperiments/increasingData.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import subprocess
+
+datasets = ["50words", "Adiac", "ArrowHead", "Beef", "BeetleFly", "BirdChicken", "Car", "CBF", "ChlorineConcentration", "CinC", "Coffee", "Computers", "Cricket", "Cricket", "Cricket", "DiatomSizeReduction", "DistalPhalanxOutlineAgeGroup", "DistalPhalanxOutlineCorrect", "DistalPhalanxTW", "Earthquakes", "ECG200", "ECG5000", "ECGFiveDays", "ElectricDevices", "FaceAll", "FaceFour", "FacesUCR", "FISH", "FordA", "FordB", "Gun", "Ham", "HandOutlines", "Haptics", "Herring", "InlineSkate", "InsectWingbeatSound", "ItalyPowerDemand", "LargeKitchenAppliances", "Lighting2", "Lighting7", "MALLAT", "Meat", "MedicalImages", "MiddlePhalanxOutlineAgeGroup", "MiddlePhalanxOutlineCorrect", "MiddlePhalanxTW", "MoteStrain", "NonInvasiveFatalECG", "NonInvasiveFatalECG", "OliveOil", "OSULeaf", "PhalangesOutlinesCorrect", "Phoneme", "Plane", "ProximalPhalanxOutlineAgeGroup", "ProximalPhalanxOutlineCorrect", "ProximalPhalanxTW", "RefrigerationDevices", "ScreenType", "ShapeletSim", "ShapesAll", "SmallKitchenAppliances", "SonyAIBORobotSurface", "SonyAIBORobotSurfaceII", "StarLightCurves", "Strawberry", "SwedishLeaf", "Symbols", "synthetic", "ToeSegmentation1", "ToeSegmentation2", "Trace", "TwoLeadECG", "Two", "UWaveGestureLibraryAll", "uWaveGestureLibrary", "uWaveGestureLibrary", "uWaveGestureLibrary", "wafer", "Wine", "WordsSynonyms", "Worms", "WormsTwoClass", "yoga", "MNIST_all"]
+
+LBR = 0.98
+q = 1.96
+Kexp = 2
+work = "REUSE"
+opt = "NOOPTIMIZE"
+
+proc = "java -Xms6g ${JAVA_OPTS} -cp 'assembly/target/*:core/target/classes:frontend/target/classes:contrib/target/classes' macrobase.analysis.stats.optimizer.experiments.IncreasingDatasizeExperiments %s %f %f %d %s %s"
+comp = "mvn compile -Dmaven.test.skip=true"
+package = "mvn package -DskipTests"
+subprocess.call(comp, shell=True)
+subprocess.call(package, shell=True)
+
+for dataset in datasets:
+    subprocess.call(proc % (dataset, LBR, q, Kexp, work, opt), shell=True)
