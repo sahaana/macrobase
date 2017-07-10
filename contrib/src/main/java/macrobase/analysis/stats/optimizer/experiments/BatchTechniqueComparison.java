0be842e02a322afda80a0ba87a4ef2626b13ca62
Sahaana <sahaana.suri@gmail.com>
2017-07-09 22:24:09 -0700

    comment out fft

--- contrib/src/main/java/macrobase/analysis/stats/optimizer/experiments/BatchTechniqueComparison.java
+++ contrib/src/main/java/macrobase/analysis/stats/optimizer/experiments/BatchTechniqueComparison.java
@@ -61,7 +61,7 @@ public class BatchTechniqueComparison extends Experiment {
         List<Datum> data = getData(dataset);
 
         PAASkiingDROP paaDrop = new PAASkiingDROP(conf, qThresh, lbr);
-        FFTSkiingDROP fftDrop = new FFTSkiingDROP(conf, qThresh, lbr);
+        //FFTSkiingDROP fftDrop = new FFTSkiingDROP(conf, qThresh, lbr);
         //RPSkiingDROP rpSkiingDROP = new RPSkiingDROP(conf, qThresh, lbr);
         PCASkiingDROP pcaDrop = new PCASkiingDROP(conf, qThresh, lbr, PCASkiingOptimizer.PCAAlgo.SVD);
         PCASkiingDROP pcaTroppDrop = new PCASkiingDROP(conf, qThresh, lbr, PCASkiingOptimizer.PCAAlgo.TROPP);
@@ -70,14 +70,14 @@ public class BatchTechniqueComparison extends Experiment {
 
 
         paaResults = paaDrop.genBasePlots(data);
-        fftResults = fftDrop.genBasePlots(data);
+        //fftResults = fftDrop.genBasePlots(data);
         pcaResults = pcaDrop.genBasePlots(data);
         pcaTroppResults = pcaTroppDrop.genBasePlots(data);
         pcaFastResults = pcaFastDrop.genBasePlots(data);
         //rpResults = rpSkiingDROP.genBasePlots(data);
 
         mapDoubleToCSV(paaResults.get("LBR"), LBROutFile(dataset,qThresh,"PAA", date));
-        mapDoubleToCSV(fftResults.get("LBR"), LBROutFile(dataset,qThresh, "FFT", date));
+        //mapDoubleToCSV(fftResults.get("LBR"), LBROutFile(dataset,qThresh, "FFT", date));
         //mapDoubleToCSV(rpResults.get("LBR"),  LBROutFile(dataset,qThresh, "RP", date));
         mapDoubleToCSV(pcaResults.get("LBR"), LBROutFile(dataset,qThresh, "PCASVD", date));
         mapDoubleToCSV(pcaTroppResults.get("LBR"), LBROutFile(dataset,qThresh, "PCATROPP", date));
@@ -85,7 +85,7 @@ public class BatchTechniqueComparison extends Experiment {
 
 
         mapDoubleToCSV(paaResults.get("time"), timeOutFile(dataset,qThresh,"PAA", date));
-        mapDoubleToCSV(fftResults.get("time"), timeOutFile(dataset,qThresh, "FFT", date));
+        //mapDoubleToCSV(fftResults.get("time"), timeOutFile(dataset,qThresh, "FFT", date));
         //mapDoubleToCSV(rpResults.get("time"),  timeOutFile(dataset,qThresh, "RP", date));
         mapDoubleToCSV(pcaResults.get("time"), timeOutFile(dataset,qThresh, "PCASVD", date));
         mapDoubleToCSV(pcaTroppResults.get("time"), timeOutFile(dataset,qThresh, "PCATROPP", date));
